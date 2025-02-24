63d586e4bc5012b470cf78b715867f8f989d082c
fbassa <fbassa98@gmail.com>
2025-02-24 14:52:11 +0000

    linear regression
    code of linear regression

--- regression.py
+++ regression.py
@@ -4,41 +4,87 @@ import numpy as np # for numpy array operations
 import matplotlib.pyplot as plt # for visualization
 import seaborn as sns
 # importing all the needed functions from scikit-learn
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split # to perform train-test split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error
+import math
 # --- END OF IMPORT SECTION ---
 
 # --- MAIN CODE ---
-#importing the dataset
+# Importing the dataset
 path_to_data = "Salary_Data.csv"
 data = pd.read_csv(path_to_data)
 
-#visualizing the dataset
-print(f"\n Here are the first 5 rows of the dataset {data.head()}")
+# Visualizing the dataset
+print(f"\nHere are the firts 5 rows of the dataset:\n{data.head()}")
 
-#separated the data in features and target
-x = data["YearsExperience"]
-y = data["Salary"]
+# Separate the data in features and target
+X = data['YearsExperience'].values.reshape(-1, 1)
+y = data['Salary'].values.reshape(-1, 1)
 
-#using a plot to visualize the data
+# Using a plot to visualize the data
 plt.title("Years of Experience vs Salary") # title of the plot
 plt.xlabel("Years of Experience") # title of x axis
 plt.ylabel("Salary") # title of y axis
-plt.scatter(data["YearsExperience"], data["Salary"], color="red") # actual plot
-sns.regplot(data = data, x = "YearsExperience", y = "Salary") #regression line
+plt.scatter(X, y, color = 'red') # actual plot
+sns.regplot(data = data, x = "YearsExperience", y = "Salary") # regression line
 plt.show() # renderize the plot to show it
 
-#splitting the dataset into training and test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.02, random_state = 101)
+# Splitting the dataset into training and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 101)
 
-#checking the train and test to proove they are 80% and 20% respectively
-print(f"\n the total x size is: {x.shape[0]}")
-print(f"\n the total x_train size is: {x_train.shape[0]}, and is the {x_train.shape[0]/x.shape[0] * 100} % of the total X")
-print(f"\n the total x_test size is: {x_test.shape[0]}, and is the {x_test.shape[0]/x.shape[0] * 100} % of the total X")
+# Checking the train and test size to proove they are 80% and 20% respectively
+print(f"\nThe total X size is: {X.shape[0]}")
+print(f"The X_train size is: {X_train.shape[0]} and is the {X_train.shape[0] / X.shape[0] * 100} % of the total X")
+print(f"The X_test size is: {X_test.shape[0]} and is the {X_test.shape[0] / X.shape[0] * 100} % of the total X")
 
+print(f"\nThe total y size is: {y.shape[0]}")
+print(f"The y_train size is: {y_train.shape[0]} and is the {y_train.shape[0] / y.shape[0] * 100} % of the total y")
+print(f"The y_test size is: {y_test.shape[0]} and is the {y_test.shape[0] / y.shape[0] * 100} % of the total y")
 
+# Visualizing data before scaling
+print(f"\n-- BEFORE SCALING -- X_train:\n{X_train[:5]}")
+print(f"\n-- BEFORE SCALING -- y_train:\n{y_train[:5]}")
+print(f"\n-- BEFORE SCALING -- X_test:\n{X_test[:5]}")
+print(f"\n-- BEFORE SCALING -- y_test:\n{y_test[:5]}")
+
+# Feature scaling
+scaler = StandardScaler()
+# we are going to scale ONLY the features (i.e. the X) and NOT the y!
+X_train_scaled = scaler.fit_transform(X_train) # fitting to X_train and transforming them
+X_test_scaled = scaler.transform(X_test) # transforming X_test. DO NOT FIT THEM!
+
+# Visualizing data after scaling
+print(f"\n-- AFTER SCALING -- X_train:\n{X_train_scaled[:5]}")
+print(f"\n-- AFTER SCALING -- y_train:\n{y_train[:5]}")
+print(f"\n-- AFTER SCALING -- X_test:\n{X_test_scaled[:5]}")
+print(f"\n-- AFTER SCALING -- y_test:\n{y_test[:5]}")
+
+# Linear Regression
+model = LinearRegression()
+
+# performing the training on the train data (i.e. X_train_scaled, y_train)
+model.fit(X_train_scaled, y_train)
+
+# predicting new values
+y_pred = model.predict(X_test_scaled)
+
+# visualizing the parameters for the Regressor after the training
+print(f"\nAfter the training, the params for the Regressor are: {model.coef_}") # the coefficient of the model
+
+# Visualizing the regression
+plt.title("Years of Experience vs Salary") # title of the plot
+plt.xlabel("Years of Experience") # title of x axis
+plt.ylabel("Salary") # title of y axis
+plt.scatter(X_test, y_test, color = 'red', label = 'Real Data') # actual plot
+plt.plot(X_test, y_pred, color = 'blue', label = 'Predicted Data') # regression line
+plt.legend() # show the legend
+plt.show()
+
+# Evaluating the model
+rmse = math.sqrt(mean_squared_error(y_test, y_pred)) # Root mean squared error
+print(f"\nRMSE: {rmse:.2f}")
 
 # --- END OF MAIN CODE ---
 
