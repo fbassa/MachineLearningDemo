cb8a5a92cac3387ba57b1d89ce8333d1cbc1598c
fbassa <fbassa98@gmail.com>
2025-02-26 08:35:35 +0000

    Create classification.py
    import section

---
+++ classification.py
@@ -0,0 +1,16 @@
+# --- IMPORT SECTION ---
+import numpy as np
+import pandas as pd # to load the data into a DataFrame
+import matplotlib.pyplot as plt # to visualize the data
+from sklearn.model_selection import train_test_split # to split the data into training and testing sets
+from sklearn.preprocessing import StandardScaler # to standardize (scale) the features (the X)
+from sklearn.ensemble import RandomForestClassifier # to use the RandomForest model
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix # to evaluate the model
+import seaborn as sns # to visualize the confusion matrix
+# --- END OF IMPORT SECTION ---
+
+
+# --- MAIN CODE ---
+
+# --- END OF MAIN CODE ---
+
